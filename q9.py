84b675ae9d8baddf921c4e56197671ddacf8aaa7
bhupeshjoshi7 <121234853+bhupeshjoshi7@users.noreply.github.com>
2023-01-27 23:59:51 +0530

    Update q9.py

--- q9.py
+++ q9.py
@@ -1,12 +1,18 @@
-str = input("Enter the string  :")
+# Get the list of words from the user
+user_list = input("Enter a list of words separated by spaces: ").split()
 
-list=[]
-count =0
-list=str.split(" ")
-print(list)
-for e in list:
-    for j in list:
-        if e==j:
-            count=count+1
-    print(count)        
-print(count)        
+# Initialize an empty dictionary to store the word counts
+word_counts = {}
+
+# Iterate through the list of words
+for word in user_list:
+    # If the word is already in the dictionary, increment the count
+    if word in word_counts:
+        word_counts[word] += 1
+    # If the word is not in the dictionary, add it with a count of 1
+    else:
+        word_counts[word] = 1
+
+# Print the word counts
+for word, count in word_counts.items():
+    print(f"{word}: {count}")
